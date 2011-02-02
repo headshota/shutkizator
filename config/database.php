7bd9d7a0e0f328126e0a7879a76b74b4fea8075d
unknown <42@.(none)>
2011-02-02 14:04:35 +0400

    Inital Commit

---
+++ config/database.php
@@ -0,0 +1,15 @@
+<?php
+class DATABASE_CONFIG {
+
+	var $default = array(
+		'driver' => 'mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => '',
+		'database' => 'shutkizator',
+		'prefix' => 'sh_',
+		'encoding' => 'UTF-8'
+	);
+}
+?>
