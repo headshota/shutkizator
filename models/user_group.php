82f6868c22cb53a210034b63953f856e3da6b9fe
Shota Bakuradze <shotabakuradze@gmail.com>
2011-04-12 15:55:18 +0400

    Added requester functionality to UserGroup Model

--- models/user_group.php
+++ models/user_group.php
@@ -3,6 +3,7 @@ class UserGroup extends AppModel {
 	var $name = 'UserGroup';
 	var $displayField = 'name';
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
+	var $actsAs = array('Acl' => array('type' => 'requester'));
 
 	var $hasMany = array(
 		'User' => array(
@@ -19,6 +20,21 @@ class UserGroup extends AppModel {
 			'counterQuery' => ''
 		)
 	);
+	
+	function parentNode() {
+    if (!$this->id && empty($this->data)) {
+        return null;
+    }
+    if (isset($this->data['User']['group_id'])) {
+	$groupId = $this->data['User']['group_id'];
+    } else {
+    	$groupId = $this->field('group_id');
+    }
+    if (!$groupId) {
+	return null;
+    } else {
+        return array('Group' => array('id' => $groupId));
+    }
+}
 
 }
-?>
